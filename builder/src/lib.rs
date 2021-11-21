5a01ba7ef19c186583329bcad1bbd27a00a9cdf0
Chenxing Li <ChenxingLi@users.noreply.github.com>
2021-11-21 15:01:02 +0800

    02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,45 @@
-#![allow(unused)]
-
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
-    // eprintln!("{:#?}", ast);
-    TokenStream::new()
+    // eprint!("{:#?}", ast);
+
+    let vis = &ast.vis;
+    let ident = &ast.ident;
+    let bident = Ident::new(&format!("{}Builder", ident), ident.span());
+
+    let fields: Vec<&Field> = if let Data::Struct(ref data_struct) = ast.data {
+        data_struct.fields.iter().collect()
+    } else {
+        unimplemented!("Only support struct");
+    };
+
+    let optionized_fields = fields.iter().cloned().map(|field| {
+        let var = field.ident.clone().expect("Except named struct");
+        let ty = field.ty.clone();
+        quote! { #var: ::std::option::Option<#ty>, }
+    });
+
+    let init_fields = fields.iter().cloned().map(|field| {
+        let var = field.ident.clone().expect("Except named struct");
+        quote! { #var: None, }
+    });
+
+    let build_struct = quote! {
+        #vis struct #bident {
+            #(#optionized_fields)*
+        }
+
+        impl #ident {
+            pub fn builder() -> #bident {
+                #bident {
+                    #(#init_fields)*
+                }
+            }
+        }
+    };
+    build_struct.into()
 }
