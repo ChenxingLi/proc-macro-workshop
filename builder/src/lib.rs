f2a67a8e37f65ef2af782a3297672e9feeaccad9
Chenxing Li <ChenxingLi@users.noreply.github.com>
2021-11-21 16:00:43 +0800

    06-optional-field

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,72 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Field, Ident};
+use syn::{
+    parse_macro_input, Data, DeriveInput, Field, GenericArgument, Ident, PathArguments,
+    PathSegment, Type,
+};
+
+enum InputType {
+    Normal(Type),
+    Optional(Type),
+}
+
+impl InputType {
+    fn from_input(ty: &Type) -> InputType {
+        match Self::detect_option(ty) {
+            Some(ty) => Self::Optional(ty),
+            None => Self::Normal(ty.clone()),
+        }
+    }
+
+    fn detect_option(ty: &Type) -> Option<Type> {
+        if let Type::Path(path) = ty {
+            if path.qself.is_some() {
+                return None;
+            }
+
+            let path_segs: Vec<&PathSegment> = path.path.segments.iter().collect();
+            let idents: Vec<String> = path_segs
+                .iter()
+                .map(|seg| format!("{}", seg.ident))
+                .collect();
+
+            let option_seg = if idents.len() == 1 && idents[0] == "Option" {
+                &path_segs[0].arguments
+            } else if idents.len() == 3
+                && idents[0] == "std"
+                && idents[1] == "option"
+                && idents[2] == "Option"
+            {
+                &path_segs[2].arguments
+            } else {
+                return None;
+            };
+
+            if let PathArguments::AngleBracketed(generic_args) = option_seg {
+                assert_eq!(
+                    generic_args.args.len(),
+                    1,
+                    "std::option::Option<T> has one generic param"
+                );
+                if let GenericArgument::Type(ty) = generic_args.args.first().unwrap() {
+                    return Some(ty.clone());
+                } else {
+                    unreachable!("In std::option::Option<T>, T should be a type.");
+                }
+            } else {
+                unreachable!("std::option::Option<T> should be AngleBracketed");
+            }
+        }
+        None
+    }
+
+    fn into_type(self) -> Type {
+        match self {
+            InputType::Normal(ty) => ty,
+            InputType::Optional(ty) => ty,
+        }
+    }
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -19,7 +85,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let optionized_fields = fields.iter().cloned().map(|field| {
         let var = field.ident.clone().expect("Except named struct");
-        let ty = field.ty.clone();
+        let ty = InputType::from_input(&field.ty).into_type();
         quote! { #var: ::std::option::Option<#ty>, }
     });
 
@@ -30,7 +96,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let setter = fields.iter().cloned().map(|field| {
         let var = field.ident.clone().expect("Except named struct");
-        let ty = field.ty.clone();
+        let ty = InputType::from_input(&field.ty).into_type();
         quote! {
             fn #var(&mut self, #var: #ty) -> &mut Self {
                 self.#var = Some(#var);
@@ -41,8 +107,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let clone_build_fields = fields.iter().cloned().map(|field| {
         let var = field.ident.clone().expect("Except named struct");
-        quote! {
-            #var: self.#var.clone().ok_or(concat!(stringify!(#var), " has not been set."))?,
+        match InputType::from_input(&field.ty) {
+            InputType::Normal(_) => quote! {
+                #var: self.#var.clone().ok_or(concat!(stringify!(#var), " has not been set."))?,
+            },
+            InputType::Optional(_) => quote! {
+                #var: self.#var.clone(),
+            },
         }
     });
 
