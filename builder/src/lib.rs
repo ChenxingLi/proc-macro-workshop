e64d89f12b9870258f33f468445a2b211e207240
Chenxing Li <ChenxingLi@users.noreply.github.com>
2021-11-21 14:34:45 +0800

    01-parse

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,12 @@
+#![allow(unused)]
+
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let ast = parse_macro_input!(input as DeriveInput);
+    // eprintln!("{:#?}", ast);
+    TokenStream::new()
 }
